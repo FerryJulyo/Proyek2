c694fceb1ae72c035930288ae696071c6c578cf2
M. Ferry Julyo <ferryjulyo86@gmail.com>
2020-11-05 12:25:51 +0700

    update 12:25

--- Shopping-App-master/Shopping-App-master/app/src/main/java/com/example/Tomat/Admin/MainActivity.java
+++ Shopping-App-master/Shopping-App-master/app/src/main/java/com/example/Tomat/Admin/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_admin_home);
 
         Button btn = (Button) findViewById(R.id.button2);
         ton3 = (Button) findViewById(R.id.button3);
