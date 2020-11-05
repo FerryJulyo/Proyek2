00d20f082550198dd893c87bd8cc096f1d267e05
M. Ferry Julyo <ferryjulyo86@gmail.com>
2020-11-05 13:07:28 +0700

    Update NotifActivity.java

--- Shopping-App-master/Shopping-App-master/app/src/main/java/com/example/Tomat/Pembeli/NotifActivity.java
+++ Shopping-App-master/Shopping-App-master/app/src/main/java/com/example/Tomat/Pembeli/NotifActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.Tomat.R;
 
 public class NotifActivity extends AppCompatActivity {
-    private int waktu_loading=5000;
+//    private int waktu_loading=5000;
 
     //4000=4 detik
 
@@ -17,16 +17,16 @@ public class NotifActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                //setelah loading maka akan langsung berpindah ke home activity
-                Intent Home=new Intent(NotifActivity.this, HomeActivity.class);
-                startActivity(Home);
-                finish();
-
-            }
-        },waktu_loading);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                //setelah loading maka akan langsung berpindah ke home activity
+//                Intent Home=new Intent(NotifActivity.this, HomeActivity.class);
+//                startActivity(Home);
+//                finish();
+//
+//            }
+//        },waktu_loading);
     }
 }
