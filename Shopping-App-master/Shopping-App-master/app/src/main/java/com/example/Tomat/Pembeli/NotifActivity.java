6795e291f960d8e832ffe62a0064378e0a8a9e69
M. Ferry Julyo <ferryjulyo86@gmail.com>
2020-11-05 12:54:09 +0700

    Update NotifActivity.java

--- Shopping-App-master/Shopping-App-master/app/src/main/java/com/example/Tomat/Pembeli/NotifActivity.java
+++ Shopping-App-master/Shopping-App-master/app/src/main/java/com/example/Tomat/Pembeli/NotifActivity.java
@@ -16,8 +16,6 @@ public class NotifActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_notification);
         new Handler().postDelayed(new Runnable() {
             @Override
